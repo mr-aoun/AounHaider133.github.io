33f8349c1f746bf4c565d64b3b4ff8c53e874017
Mr. Aoun Haider <124600838+AounHaider133@users.noreply.github.com>
2024-06-23 19:04:18 +0500

    Add files via upload

---
+++ Semester_Material/Semester_5/Data_Communication_And_Computer_Networks/Lab/Assignment_3(Multithread_Client_Server_Architecture/HTTP Server.py
@@ -0,0 +1,46 @@
+from socket import *
+import threading as th
+
+def handle_client(connectionSocket):
+
+    try:
+        # Receive HTTP request from browser
+        message = connectionSocket.recv(1024)
+        print(f"\n{message.decode()}")
+
+        # Open file mentioned in the first line of the HTTP request header
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read()
+
+        #Send one HTTP header line into socket
+        connectionSocket.send(("HTTP/1.1 200 OK\r\n\r\n").encode())
+
+        #Send the content of the requested file to the client
+        connectionSocket.send(outputdata.encode())
+        connectionSocket.close()
+
+    except IOError:
+        #Send response message for file not found
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n")
+    finally:
+        #Close client socket
+        connectionSocket.close()
+
+print()
+print(gethostbyname(gethostname()))
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+#Prepare a sever socket
+serverSocket.bind((gethostname(), 5432))
+serverSocket.listen(1)
+
+while True:
+    #Establish the connection
+    print("\n\nReady to serve...")
+    connectionSocket, address = serverSocket.accept()
+    client_thread = th.Thread(target=handle_client,args=(connectionSocket,))
+    client_thread.start()
+    
+serverSocket.close()
